d6f8c8add0d43143d7542c762a31b5efdaf1889b
Temirlan Raiymbek <raiymbektemirlan25@gmail.com>
2025-06-13 10:48:25 +0500

    add tanstack

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,5 +1,9 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
+const queryCLient = new QueryClient()
 
 export function Provider({children}: PropsWithChildren){
-
+    return(
+        <QueryClientProvider client={queryCLient}>{children}</QueryClientProvider>
+    )
 }
